a0f80694636879ca48c8ea2fa3b3f123b8640526
llite22 <llite2@yandex.ru>
2024-10-18 13:26:41 +0300

    route + controller

---
+++ app/Http/Controllers/WorkerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WorkerController extends Controller
+{
+    public function index()
+    {
+        return 'hello index';
+    }
+
+    public function show()
+    {
+        return 'hello show';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/workers', [\App\Http\Controllers\WorkerController::class, 'index']);
+
+Route::get('/workers/show', [\App\Http\Controllers\WorkerController::class, 'show']);
