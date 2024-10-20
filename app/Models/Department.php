3294dccb32560514903248f546c665cf48d86523
llite22 <llite2@yandex.ru>
2024-10-20 23:54:48 +0300

    Factory и Seeder

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class Department extends Model
 {
+    use HasFactory;
+
     protected $table = 'departments';
 
     protected $guarded = false;
--- app/Models/Worker.php
+++ app/Models/Worker.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -12,6 +13,8 @@ use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Worker extends Model
 {
+    use HasFactory;
+
     protected $table = 'workers';
 
     protected $guarded = false;
