cc59347e6cf4072deb7fd9529beab9385bdd1e28
llite22 <llite2@yandex.ru>
2024-10-21 16:46:21 +0300

    authorization

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/workers', [\App\Http\Controllers\WorkerController::class, 'index'])->name('worker.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/workers/create', [\App\Http\Controllers\WorkerController::class, 'create'])->name('worker.create');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::post('/workers', [\App\Http\Controllers\WorkerController::class, 'store'])->name('worker.store');
-
-Route::get('/workers/{worker}', [\App\Http\Controllers\WorkerController::class, 'show'])->name('worker.show');
-
-
-Route::get('/workers/{worker}/edit', [\App\Http\Controllers\WorkerController::class, 'edit'])->name('worker.edit');
-
-Route::patch('/workers/{worker}', [\App\Http\Controllers\WorkerController::class, 'update'])->name('worker.update');
-
-Route::delete('/workers/{worker}', [\App\Http\Controllers\WorkerController::class, 'delete'])->name('worker.delete');
+require __DIR__.'/auth.php';
