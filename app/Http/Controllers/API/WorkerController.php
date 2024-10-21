2c132661954c5f871bc721c7a5a0d1567da5068d
llite22 <llite2@yandex.ru>
2024-10-21 21:29:36 +0300

    Class Resource

--- app/Http/Controllers/API/WorkerController.php
+++ app/Http/Controllers/API/WorkerController.php
@@ -3,18 +3,16 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\WorkerResource;
 use App\Models\Worker;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
-    public function index(): JsonResponse
+    public function index()
     {
         $workers = Worker::all();
 
-        return response()->json([
-            'data' => $workers
-        ]);
+        return WorkerResource::collection($workers);
     }
 }
